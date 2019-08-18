0fd7aaea0d338cdf041700b4fadf92db9a3e1c0a
Tsukamoto <49139020+TsuMakoto@users.noreply.github.com>
2019-08-18 14:05:31 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+mkdir ~/.config && mkdir ~/.config/nvim && mkdir ~/.config/colors
 ln -s ~/.dotfiles/vim/init.vim ~/.config/nvim/init.vim
 ln -s ~/.dotfiles/vim/colors ~/.config/nvim/colors
 ln -s ~/.dotfiles/zshrc/.zshrc ~/.zshrc
