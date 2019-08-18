39ec62818d89be9d3af05d55138201294e26d82f
TsuMakoto <tsukamoto@fusic.co.jp>
2019-08-18 23:45:56 +0900

    Change symbol link.

--- init.sh
+++ init.sh
@@ -1,5 +1,3 @@
 mkdir ~/.config
-mkdir ~/.config/nvim
-ln -s ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-ln -s ~/.dotfiles/nvim/colors ~/.config/nvim/colors
+ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/zshrc/.zshrc ~/.zshrc
