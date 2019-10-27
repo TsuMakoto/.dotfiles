9ac7b16af56bd673f76415f7e571a5ddf21f2052
TsuMakoto <tsukamoto@fusic.co.jp>
2019-10-27 16:28:20 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 mkdir ~/.config
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/kitty ~/.config/kitty
+ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/.dotfiles/zshrc/.zshrc ~/.zshrc
