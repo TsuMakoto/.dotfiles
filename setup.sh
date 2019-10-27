89f41a9719bb2778da2bd0185b8fe04c45384129
TsuMakoto <tsukamoto@fusic.co.jp>
2019-10-27 16:44:44 +0900

    Add ctags

--- setup.sh
+++ setup.sh
@@ -7,4 +7,8 @@ ln -s ~/.dotfiles/kitty ~/.config/kitty
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
 
+# zshrc
 ln -s ~/.dotfiles/zshrc/.zshrc ~/.zshrc
+
+# ctags
+ln -s ~/.dotfiles/.ctags ~/.ctags
