560e60ce8caf7d417b62dbebd5bec30abf7ee23b
TsuMakoto <tsukamoto@fusic.co.jp>
2019-10-27 16:37:02 +0900

    Add gitconfig

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
+# configration
 mkdir ~/.config
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/kitty ~/.config/kitty
+
+# git
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
+
 ln -s ~/.dotfiles/zshrc/.zshrc ~/.zshrc
